7c662c276babf0575e8784617ac5972d40126e9a
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-07-19 13:38:21 -0700

    updated

--- src/Component.php
+++ src/Component.php
@@ -15,6 +15,7 @@
 namespace Backdrop\Customize;
 
 use Backdrop\Contracts\Bootable;
+use Backdrop\Customize\Contracts\Customize;
 
 use WP_Customize_Manager;
 
@@ -24,7 +25,14 @@ use WP_Customize_Manager;
  * @since  1.0.0
  * @access public
  */
-interface Component extends Bootable {
+class Component implements Customize {
+
+    public function boot() {
+        add_action( 'customize_register', [ $this, 'panels' ] );
+		add_action( 'customize_register', [ $this, 'sections' ] );
+		add_action( 'customize_register', [ $this, 'settings' ] );
+		add_action( 'customize_register', [ $this, 'controls' ] );
+    }
 
     /**
      * Add our panels for customizer.
@@ -34,7 +42,21 @@ interface Component extends Bootable {
      * @param  WP_Customize_Manager $manager
      * @return void
      */
-    public function panels( WP_Customize_Manager $manager );
+    public function panels( WP_Customize_Manager $manager ) {
+		$panels = [
+			'theme_global'  => esc_html__( 'Theme: Global', 'silver-quantum' ),
+			'theme_header'  => esc_html__( 'Theme: Header', 'silver-quantum' ),
+			'theme_content' => esc_html__( 'Theme: Content', 'silver-quantum' ),
+			'theme_footer'  => esc_html__( 'Theme: Footer', 'silver-quantum' )
+		];
+
+		foreach ( $panels as $panel => $label ) {
+			$manager->add_panel( $panel, [
+				'title'    => $label,
+				'priority' => 100
+			] );
+		}
+    }
 
     /**
      * Add our sections for customizer.
@@ -44,7 +66,9 @@ interface Component extends Bootable {
      * @param  WP_Customize_Manager $manager
      * @return void
      */
-    public function sections( WP_Customize_Manager $manager );
+    public function sections( WP_Customize_Manager $manager ) {
+
+    }
 
     /**
      * Add our settings for customizer.
@@ -54,7 +78,9 @@ interface Component extends Bootable {
      * @param  WP_Customize_Manager $manager
      * @return void
      */
-    public function settings( WP_Customize_Manager $manager );
+    public function settings( WP_Customize_Manager $manager ) {
+
+    }
 
     /**
      * Add our controls for customizer.
@@ -64,5 +90,7 @@ interface Component extends Bootable {
      * @param  WP_Customize_Manager $manager
      * @return void
      */
-    public function controls( WP_Customize_Manager $manager );
+    public function controls( WP_Customize_Manager $manager ) {
+
+    }
 }
