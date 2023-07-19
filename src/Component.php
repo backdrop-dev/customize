29d9950abc7887b6531a5944a208c2affebfe560
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-07-19 14:10:53 -0700

    updated

--- src/Component.php
+++ src/Component.php
@@ -24,14 +24,7 @@ use WP_Customize_Manager;
  * @since  1.0.0
  * @access public
  */
-class Component implements Bootable {
-
-    public function boot() {
-        add_action( 'customize_register', [ $this, 'panels' ] );
-				add_action( 'customize_register', [ $this, 'sections' ] );
-				add_action( 'customize_register', [ $this, 'settings' ] );
-				add_action( 'customize_register', [ $this, 'controls' ] );
-    }
+interface Component extends Bootable {
 
     /**
      * Add our panels for customizer.
@@ -41,19 +34,35 @@ class Component implements Bootable {
      * @param  WP_Customize_Manager $manager
      * @return void
      */
-    public function panels( WP_Customize_Manager $manager ) {
-		$panels = [
-			'theme_global'  => esc_html__( 'Theme: Global',  'backdrop' ),
-			'theme_header'  => esc_html__( 'Theme: Header',  'backdrop' ),
-			'theme_content' => esc_html__( 'Theme: Content', 'backdrop' ),
-			'theme_footer'  => esc_html__( 'Theme: Footer',  'backdrop' )
-		];
+    public function panels( WP_Customize_Manager $manager );
 
-		foreach ( $panels as $panel => $label ) {
-			$manager->add_panel( $panel, [
-				'title'    => $label,
-				'priority' => 100
-			] );
-		}
-    }
+    /**
+     * Add our sections for customizer.
+     *
+     * @since  1.0.0
+     * @access public
+     * @param  WP_Customize_Manager $manager
+     * @return void
+     */
+    public function sections( WP_Customize_Manager $manager );
+
+    /**
+     * Add our settings for customizer.
+     *
+     * @since  1.0.0
+     * @access public
+     * @param  WP_Customize_Manager $manager
+     * @return void
+     */
+    public function settings( WP_Customize_Manager $manager );
+
+    /**
+     * Add our controls for customizer.
+     *
+     * @since  1.0.0
+     * @access public
+     * @param  WP_Customize_Manager $manager
+     * @return void
+     */
+    public function controls( WP_Customize_Manager $manager );
 }
