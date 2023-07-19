885ab113d01517aa9d1f72b2ec83e7178b340cfa
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-07-19 13:39:52 -0700

    updated

--- src/Provider.php
+++ src/Provider.php
@@ -23,11 +23,11 @@ class Provider extends ServiceProvider {
 	 * @access public
 	 * @return void
 	 */
-	public function register(): void {
+	public function register() {
 		$this->app->singleton( 'backdrop/customize', Component::class );
 	}
 
-	public function boot(): void {
+	public function boot() {
 		$this->app->resolve( 'backdrop/customize' )->boot();
 	}
 }
