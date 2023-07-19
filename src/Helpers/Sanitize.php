10d6b8a15065616d859f5ebb07d433f197b2f5cd
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-07-19 09:49:39 -0700

    make sure the header section is set correctly with the correct information.

--- src/Helpers/Sanitize.php
+++ src/Helpers/Sanitize.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Backdrop Core ( Sanitize.php )
+ * Sanitize Helpers
  *
- * @package   Backdrop
- * @author      Benjamin Lu ( benlumia007@gmail.com )
- * @copyright Copyright (C) 2019-2020. Benjamin Lu
+ * @package   Backdrop Customize
+ * @author    Benjamin Lu ( benlumia007@gmail.com )
+ * @copyright 2019-2023. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/backdrop-dev/customize
  */
 
 /**
