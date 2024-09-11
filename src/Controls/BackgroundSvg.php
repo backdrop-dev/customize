fe9342d7b36b7b39d1fd81149307f510305f5de3
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2024-09-10 22:54:53 -0700

    add BackgroundSvg.php

---
+++ src/Controls/BackgroundSvg.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Radio SVG customize control.
+ *
+ * This control allows developers to create a list of SVG radio inputs.
+ *
+ * @package   Prismatic Customize
+ */
+
+namespace Backdrop\Customize\Controls;
+
+/**
+ * Radio SVG customize control.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class BackgroundSvg extends Control {
+
+    /**
+     * The type of customize control being rendered.
+     *
+     * @access public
+     * @since  1.0.0
+     * @var    string
+     */
+    public $type = 'backdrop-image-radio-svg';
+
+    /**
+     * Loads the template for SVG radio control
+     *
+     * @return void
+     */
+    protected function render_content() {
+        if ( empty( $this->choices ) ) {
+            return;
+        }
+
+        $name = '_customize-radio-' . $this->id;
+        ?>
+        <span class="customize-control-title">
+			<?php echo esc_html( $this->label ); ?>
+		</span>
+        <?php if ( ! empty( $this->description ) ) : ?>
+            <span class="description customize-control-description"><?php echo esc_html( $this->description ); ?></span>
+        <?php endif; ?>
+
+        <div id="input_<?php echo esc_attr( $this->id ); ?>" class="svg-patterns">
+            <?php foreach ( $this->choices as $value => $svg ) : ?>
+                <label for="<?php echo esc_attr( $this->id . '_' . $value ); ?>">
+                    <input class="svg-select" type="radio" value="<?php echo esc_attr( $value ); ?>" id="<?php echo esc_attr( $this->id . '_' . $value ); ?>" name="<?php echo esc_attr( $name ); ?>"
+                        <?php
+                        $this->link();
+                        checked( $this->value(), esc_attr( $value ) );
+                        ?>
+                    >
+                    <!-- Encode the SVG to use as a background-image -->
+                    <span class="svg-preview" style="
+                        background-image: url('data:image/svg+xml,<?php echo rawurlencode( $svg ); ?>');
+                    "></span>
+                </label>
+            <?php endforeach; ?>
+        </div>
+        <?php
+    }
+}
